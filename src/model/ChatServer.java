80f3dec0337dd3cfd838080794b458f93e566562
JustOverFR <jeanjuste.lefebvre@gmail.com>
2023-03-28 15:57:42 +0200

    story 1 finito

--- src/model/ChatServer.java
+++ src/model/ChatServer.java
@@ -3,12 +3,14 @@ package model;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 
 /**
  * Classe ChatServer représentant le serveur de chat.
  */
 public class ChatServer {
-    private ServerSocket serverSocket;
+    private static ServerSocket serverSocket;
+    private static ArrayList<ClientHandler> clients = new ArrayList<ClientHandler>();
 
     /**
      * Constructeur de la classe ChatServer.
@@ -27,6 +29,31 @@ public class ChatServer {
     }
 
     /**
+     * Ajoute un client à la liste des clients connectés.
+     *
+     * @param client Le client à ajouter
+     */
+    public void addClient(ClientHandler client) {
+        clients.add(client);
+    }
+
+    /**
+     * Supprime un client de la liste des clients connectés.
+     *
+     * @param client Le client à supprimer
+     */
+    public void removeClient(ClientHandler client) {
+        clients.remove(client);
+    }
+
+    public void broadcastMessage(String message) {
+        // Envoie le message à tous les clients connectés
+        for (ClientHandler client : clients) {
+            client.sendMessage(message);
+        }
+    }
+
+    /**
      * Méthode pour démarrer le serveur et accepter les connexions entrantes des
      * clients.
      */
@@ -38,6 +65,19 @@ public class ChatServer {
                 // connexion établie avec le client
                 Socket clientSocket = serverSocket.accept();
                 System.out.println("Un nouveau client s'est connecté : " + clientSocket.getInetAddress());
+
+                // Crée un nouveau ClientHandler pour gérer les messages du client
+                ClientHandler clientHandler = new ClientHandler(this, clientSocket);
+
+                // Ajoute le ClientHandler à la liste des clients connectés
+                addClient(clientHandler);
+
+                // Crée un nouveau thread pour gérer les messages du client
+                Thread clientThread = new Thread(clientHandler);
+
+                // Démarrer le thread
+                clientThread.start();
+
             } catch (IOException e) {
                 // Affiche la pile d'appels des méthodes qui ont conduit à l'exception
                 e.printStackTrace();
--- src/model/ClientHandler.java
+++ src/model/ClientHandler.java
@@ -1,17 +1,116 @@
 package model;
 
 import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 
-public class ClientHandler {
+public class ClientHandler implements Runnable {
     /* --------------------- Envoie les messages aux clients -------------------- */
     Socket socket;
     PrintWriter printWriter;
     // Lis les messages reçus du client
     BufferedReader bufferedReader;
+    ChatServer chatServer;
 
-    ClientHandler(Socket socket) {
+    public ClientHandler(ChatServer chatServer, Socket socket) {
+        this.chatServer = chatServer;
         this.socket = socket;
+
+        try {
+            // Initialise le PrintWriter avec le flux de sortie du socket
+            printWriter = new PrintWriter(socket.getOutputStream());
+            // Initialise le BufferedReader avec le flux d'entrée du socket
+            bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        } catch (IOException e) {
+            // Affiche la pile d'appels des méthodes qui ont conduit à l'exception
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Envoie un message au client.
+     *
+     * @param message Le message à envoyer.
+     */
+    public void sendMessage(String message) {
+        // Envoie le message au client
+        printWriter.println(message);
+        /*
+         * Lorsque tu envoies des données avec PrintWriter, elles sont parfois mises en
+         * mémoire tampon pour améliorer les performances d'écriture. Cela signifie que
+         * les données ne sont pas toujours immédiatement envoyées au destinataire.
+         * 
+         * La méthode flush() force l'envoi immédiat de toutes les données mises en
+         * mémoire tampon.
+         */
+        printWriter.flush();
+    }
+
+    public void disconnect() {
+        try {
+
+            // Ferme le flux de sortie du socket
+            if (printWriter != null) {
+                printWriter.close();
+            }
+
+            // Ferme le flux d'entrée du socket
+            if (bufferedReader != null) {
+                bufferedReader.close();
+            }
+
+            // Ferme le socket
+            if (socket != null) {
+                socket.close();
+            }
+
+            // Supprime le ClientHandler de la liste des clients connectés
+            chatServer.removeClient(this);
+
+        } catch (IOException e) {
+            // Affiche la pile d'appels des méthodes qui ont conduit à l'exception
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Exécute le code de gestion des messages dans un nouveau thread.
+     * Cette méthode est appelée automatiquement lorsqu'un nouvel objet Thread est
+     * démarré avec une instance de ClientHandler.
+     */
+    @Override
+    public void run() {
+        // Le code à exécuter dans un nouveau thread sera placé ici
+        try {
+            while (true) {
+                // Lit le message envoyé par le client
+                String message = bufferedReader.readLine();
+
+                // Si le message est vide, celui-ci n'est pas partagé
+                if (message == null || message.isEmpty()) {
+                    continue;
+                }
+
+                // Affiche le message reçu
+                System.out.println("Message reçu : " + message);
+
+                // Si le message est /quit, le client est déconnecté
+                if (message.equals("/quit")) {
+                    break;
+                }
+
+                // Envoie le message à tous les clients connectés
+                chatServer.broadcastMessage(message);
+
+            }
+        } catch (IOException e) {
+            // Affiche la pile d'appels des méthodes qui ont conduit à l'exception
+            e.printStackTrace();
+
+        } finally {
+            disconnect();
+        }
     }
 }
